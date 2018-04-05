fd5c120183334fcb2017d2ecfd0e7f3ed5a4507c
Aleksander Turberville <aturberv@gmail.com>
2018-04-05 09:48:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const mnode = {};
-mnode.asyncMonitor = require('./async_perf_hooks.js')
-mnode.pkgMonitor = require('./packageMonitor.js')
-
-module.exports = mnode;
-
-
 const asyncMonitor = require('./async_perf_hooks.js')
 const pkgMonitor = require('./packageMonitor.js')
 
